adc589135a7102fdaa2d5a2d8764dfa126fece94
suifeng <369202865@qq.com>
2025-06-09 02:17:38 +0800

    dev: 实现了本地的插件类型的加载读取，待优化

--- mosaic-user-test-project/src/main/java/io/github/tml/systemLog/api/SystemErrorCubeApi.java
+++ mosaic-user-test-project/src/main/java/io/github/tml/systemLog/api/SystemErrorCubeApi.java
@@ -1,6 +1,6 @@
 package io.github.tml.systemLog.api;
 
-import io.github.tml.mosaic.cube.Cube;
+import io.github.tml.mosaic.core.tools.guid.GUID;
 import io.github.tml.mosaic.cube.ExtensionPackage;
 import io.github.tml.mosaic.cube.MExtension;
 import io.github.tml.mosaic.cube.MExtensionPackage;
@@ -8,23 +8,20 @@ import io.github.tml.systemLog.cube.SystemLogCube;
 
 import static io.github.tml.systemLog.config.Constant.PLUGIN_ID;
 
-
 @MExtensionPackage(value = "system.error",
         name = "系统日志错误输出包",
         version = "1.0.1",
         cubeId = PLUGIN_ID)
-public class SystemErrorCubeApi extends ExtensionPackage {
+public class SystemErrorCubeApi extends ExtensionPackage<SystemLogCube> {
 
-    SystemLogCube systemLogCube;
 
-    public SystemErrorCubeApi(Cube cube) {
-        super(cube);
-        this.systemLogCube = (SystemLogCube) cube;
+    public SystemErrorCubeApi(SystemLogCube cube, GUID guid) {
+        super(cube, guid);
     }
 
     @MExtension(value = "error", name = "错误输出", priority = 1)
     public void error(String msg) {
-        systemLogCube.error(msg);
+        cube.error(msg);
     }
 
 }
