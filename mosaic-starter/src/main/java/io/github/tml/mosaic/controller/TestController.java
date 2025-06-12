8ad425f5ba66b2b9b5a8eb935562179e527868ad
suifeng <369202865@qq.com>
2025-06-12 16:06:56 +0800

    [dev] 前端框架初始化

--- mosaic-starter/src/main/java/io/github/tml/mosaic/controller/TestController.java
+++ mosaic-starter/src/main/java/io/github/tml/mosaic/controller/TestController.java
@@ -1,6 +1,6 @@
 package io.github.tml.mosaic.controller;
 
-import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson.JSON;
 import io.github.tml.mosaic.entity.DTO.HotSwapRequestDTO;
 import io.github.tml.mosaic.entity.dto.AgentSocketRequestDTO;
 import io.github.tml.mosaic.util.ASMUtil;
