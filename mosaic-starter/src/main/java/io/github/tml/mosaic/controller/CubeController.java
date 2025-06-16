1b8c1c931f2cfcd8562b359b83a803c54d8dc218
suifeng <369202865@qq.com>
2025-06-16 23:51:19 +0800

    [dev] 【API】Cubelist接口开发（apifox，规范）

--- mosaic-starter/src/main/java/io/github/tml/mosaic/controller/CubeController.java
+++ mosaic-starter/src/main/java/io/github/tml/mosaic/controller/CubeController.java
@@ -5,7 +5,6 @@ import io.github.tml.mosaic.service.impl.CubeInfoService;
 import io.github.tml.mosaic.util.R;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
