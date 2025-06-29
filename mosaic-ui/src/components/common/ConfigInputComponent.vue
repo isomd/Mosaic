4ae819031eb3e306aa333db29cc656b628f945f4
1204992313@qq.com <1204992313@qq.com>
2025-06-29 23:22:34 +0800

    [fixes] 插件配置表单数字输入框bug修复

--- mosaic-ui/src/components/common/ConfigInputComponent.vue
+++ mosaic-ui/src/components/common/ConfigInputComponent.vue
@@ -21,7 +21,7 @@ const valueProxy = computed({
 <template>
   <v-label>{{props.label}}</v-label>
   <v-text-field variant="outlined" v-model="valueProxy" v-if="props.type === 'string'"></v-text-field>
-  <v-text-field type="number" v-model="valueProxy" controlVariant="default" variant="outlined" v-else-if="props.type==='number'"></v-text-field>
+  <v-number-input type="number" v-model="valueProxy" controlVariant="default" variant="outlined" v-else-if="props.type==='number'"></v-number-input>
   <v-switch :true-value="true" :false-value="false" inset v-model="valueProxy" v-else-if="props.type === 'boolean'" ></v-switch>
 </template>
 <style scoped lang="scss">
