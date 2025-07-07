32fde948305848417223b7e35468efabe1daec78
li <3250162035@qq.com>
2025-07-07 23:21:23 +0800

    世界domain提供获取世界组件能力

--- mosaic-starter/src/main/java/io/github/tml/mosaic/core/world/event/event/WorldEvent.java
+++ mosaic-starter/src/main/java/io/github/tml/mosaic/core/world/event/event/WorldEvent.java
@@ -1,8 +1,15 @@
 package io.github.tml.mosaic.core.world.event.event;
 
 import io.github.tml.mosaic.core.event.event.MosaicEvent;
+import io.github.tml.mosaic.core.tools.guid.GUID;
+import lombok.Data;
 
+@Data
 public abstract class WorldEvent extends MosaicEvent {
+    private GUID oldWorldId;
+
+    private GUID newWorldId;
+
     protected WorldEvent(Object source) {
         super(source);
     }
