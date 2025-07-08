bfedbc529b3b33856a7173bb372e8ad35c7d3448
welsir <1824379011@qq.com>
2025-07-08 23:54:34 +0800

    热部署模块挂载世界钩子 收敛context域功能

--- mosaic-starter/src/main/java/io/github/tml/mosaic/core/persistence/init/GlobalFlushManager.java
+++ mosaic-starter/src/main/java/io/github/tml/mosaic/core/persistence/init/GlobalFlushManager.java
@@ -1,47 +1,47 @@
-package io.github.tml.mosaic.core.persistence.init;
-
-import io.github.tml.mosaic.domain.HotSwapDomain;
-import io.github.tml.mosaic.hotSwap.HotSwapRepository;
-import io.github.tml.mosaic.hotSwap.model.HotSwapPoint;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-/**
- * @author welsir
- * @description :
- * @date 2025/6/30
- */
-@Component
-@Slf4j
-@DependsOn("mosaicHotSwapLoaderInstaller")
-public class GlobalFlushManager implements CommandLineRunner {
-
-    @Resource
-    HotSwapRepository hotSwapRepository;
-    @Resource
-    HotSwapDomain hotSwapDomain;
-    @Resource
-    ApplicationContext applicationContext;
-
-    @Override
-    public void run(String... args) throws Exception {
-        //获取热更新点存储文件并赋值到内存对象
-        List<HotSwapPoint> hotSwapPoints = hotSwapRepository.getHotSwapPoints();
-        hotSwapPoints.forEach(hotSwapDomain::setHotSwapPoint);
-        hotSwapDomain.recoverHotSwapPoint(hotSwapPoints);
-        //---------------------
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(() -> {
-            hotSwapRepository.save();
-        }, 0, 5, TimeUnit.SECONDS);
-    }
-}
+//package io.github.tml.mosaic.core.persistence.init;
+//
+//import io.github.tml.mosaic.domain.HotSwapDomain;
+//import io.github.tml.mosaic.hotSwap.HotSwapRepository;
+//import io.github.tml.mosaic.hotSwap.model.HotSwapPoint;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.DependsOn;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//import java.util.List;
+//import java.util.concurrent.Executors;
+//import java.util.concurrent.ScheduledExecutorService;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * @author welsir
+// * @description :
+// * @date 2025/6/30
+// */
+//@Component
+//@Slf4j
+//@DependsOn("mosaicHotSwapLoaderInstaller")
+//public class GlobalFlushManager implements CommandLineRunner {
+//
+//    @Resource
+//    HotSwapRepository hotSwapRepository;
+//    @Resource
+//    HotSwapDomain hotSwapDomain;
+//    @Resource
+//    ApplicationContext applicationContext;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        //获取热更新点存储文件并赋值到内存对象
+//        List<HotSwapPoint> hotSwapPoints = hotSwapRepository.getHotSwapPoints();
+//        hotSwapPoints.forEach(hotSwapDomain::setHotSwapPoint);
+//        hotSwapDomain.recoverHotSwapPoint(hotSwapPoints);
+//        //---------------------
+//        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+//        scheduler.scheduleAtFixedRate(() -> {
+//            hotSwapRepository.save();
+//        }, 0, 5, TimeUnit.SECONDS);
+//    }
+//}
